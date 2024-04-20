845c149344b9ea23572c088d1dc3651c44245c57
Avner Hattab <avner.h@nanox.vision>
2024-04-20 17:35:26 +0300

    first feature

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import {favoriteRouter} from './features/favorites';
 
 const app = express();
 app.use(express.json());
+
+app.use('/favorites', favoriteRouter);
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
 export  {app};
